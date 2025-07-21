6bfbaa62ff1f0801178b60a44a019ee8283a320d
José Noguera <juniornoguera1@gmail.com>
2025-07-21 17:36:22 -0600

    Finalizacion de programa

--- Simulador de votación con validacion cruzada.py
+++ Simulador de votación con validacion cruzada.py
@@ -1,16 +1,25 @@
 name = input("Ingrese su nombre completo\n")
 dpi_nums = input("Ingrese su número de dpi\n")
-departament = input("Ingrese el departamento donde vive\n")
+departament = input("Ingrese el departamento donde vive\n").upper()
 birth_year = int(input("Ingrese el año en que nació\n"))
+can_vote = True
 
 if birth_year < 2008 and departament == "peten" or departament == "alta verapaz":
-    print("peudes votar")
-elif birth_year < 2007:
-    print("puedes votar")
-else:
-    print("No podes votar, eres menor de 18 años")
+    can_vote = True
+
+if birth_year > 2007:
+    print("Eres menor de edad")
+    can_vote = False
 
 if len(name) < 5:
     print("Nombre no valido")
+    can_vote = False
+
 if len(dpi_nums) < 13:
     print("DPI no válido")
+    can_vote = False
+
+if can_vote:
+    print(f"Bienvenido {name}, su centro de votación está en {departament}")
+else:
+    print("No puedes votar")
