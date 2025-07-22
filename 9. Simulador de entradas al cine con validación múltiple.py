489da23a9126bc29a86888d3d6e10bf5a6466afc
José Noguera <juniornoguera1@gmail.com>
2025-07-21 23:40:35 -0600

    eleccion de variables

---
+++ 9. Simulador de entradas al cine con validación múltiple.py
@@ -0,0 +1,9 @@
+age = int(input("Introduzca su edad\n"))
+day = input("Introduzca su edad")
+student = int(input("Es estudiante?\n"
+                    "1. Si\n"
+                    "2. No\n"))
+
+normal_price = 50
+student_price = 35
+
