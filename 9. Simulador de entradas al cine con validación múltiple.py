e4d1c5dcb30aeff775f29f0da55b2f86cc5e54e1
José Noguera <juniornoguera1@gmail.com>
2025-07-21 23:57:40 -0600

    mejoras

--- 9. Simulador de entradas al cine con validación múltiple.py
+++ 9. Simulador de entradas al cine con validación múltiple.py
@@ -1,9 +1,16 @@
 age = int(input("Introduzca su edad\n"))
-day = input("Introduzca su edad")
+day = input("Introduzca el dia\n").upper
 student = int(input("Es estudiante?\n"
                     "1. Si\n"
                     "2. No\n"))
 
 normal_price = 50
 student_price = 35
+
+if age < 13:
+    print("usted no puede ver esta película")
+elif day == "miercoles":
+    print("Hoy hay 2x1")
+elif student == 1:
+    print(f"El total es de Q{student_price}")
 
